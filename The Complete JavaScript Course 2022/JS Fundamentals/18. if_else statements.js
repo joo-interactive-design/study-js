62a42edcf48b79d17ecd4fc760bf02a594c61858
joo-interactive-design <86972559+joo-interactive-design@users.noreply.github.com>
2022-04-01 06:29:34 -0400

    Update 18. if_else statements.js

--- The Complete JavaScript Course 2022/JS Fundamentals/18. if_else statements.js
+++ The Complete JavaScript Course 2022/JS Fundamentals/18. if_else statements.js
@@ -1,1 +1,27 @@
 
+const age = 19;
+const isOldEnough = age>=18;
+
+//example1:
+//boolean in (): if () is false, {} will not be executed.
+if(isOldEnough){
+  console.log('Sarah can start driving license')
+}
+
+//example2:
+if(isOldEnough){
+  const yearsLeft = 18-age;
+  console.log('Sarah is too young. Wait another ${yearsLeft} years.')
+}
+
+//example3:
+const birthYear = 1990;
+
+let century;
+if (birthday <= 2000){
+  century = 20;
+} else {
+  century = 21;
+}
+
+console.log(century)
